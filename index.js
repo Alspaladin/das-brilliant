58b7377c6a0f1eae8c1e68af73c0e20d82c183a7
ArthurStam <bumbledos@gmail.com>
2014-09-08 10:59:41 +0400

    rename server js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 
+app.set('port', (process.env.PORT || 5000))
 app.set('views', './develop/jade');
 app.set('view engine', 'jade');
 
-var server = app.listen(5000, function() {
+var server = app.listen(app.get('port'), function() {
     console.log('Listening on port %d', server.address().port);
 });
 
