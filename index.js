5d6901c40eca00b6e42fcba1c6ad8649a0094e4a
Alex Orlov <i@alexxorlovv.name>
2015-03-07 00:51:56 +0300

    index

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ var app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 5000));
 app.set('views', './develop/jade');
 app.set('view engine', 'jade');
 
